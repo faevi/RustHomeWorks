46ef65b66f1136ea0f9532937920086220b4f353
gleblevin <gleb-levin323@mail.ru>
2023-07-04 23:24:28 +0300

    Hello World again

---
+++ HelloWorld/rust_hello_world/target/debug/deps/rust_hello_world-1daf8bd8ae684310.d
@@ -0,0 +1,5 @@
+/Users/gleblevin/Projects/RustHomeWorks/HelloWorld/rust_hello_world/target/debug/deps/rust_hello_world-1daf8bd8ae684310: src/main.rs
+
+/Users/gleblevin/Projects/RustHomeWorks/HelloWorld/rust_hello_world/target/debug/deps/rust_hello_world-1daf8bd8ae684310.d: src/main.rs
+
+src/main.rs:
---
+++ HelloWorld/rust_hello_world/target/debug/deps/rust_hello_world-aa5a3c0b73f38c49.d
@@ -0,0 +1,5 @@
+/Users/gleblevin/Projects/RustHomeWorks/HelloWorld/rust_hello_world/target/debug/deps/rust_hello_world-aa5a3c0b73f38c49.rmeta: src/main.rs
+
+/Users/gleblevin/Projects/RustHomeWorks/HelloWorld/rust_hello_world/target/debug/deps/rust_hello_world-aa5a3c0b73f38c49.d: src/main.rs
+
+src/main.rs:
