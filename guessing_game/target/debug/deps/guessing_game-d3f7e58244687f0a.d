3a9e39dc4d257792f36a7f751cadd0e4c22a9c94
Gleb <94059832+faevi@users.noreply.github.com>
2023-07-14 02:27:16 +0300

    Delete guessing_game/target directory

--- guessing_game/target/debug/deps/guessing_game-d3f7e58244687f0a.d
+++
@@ -1,5 +0,0 @@
-/Users/gleblevin/Projects/RustHomeWorks/guessing_game/target/debug/deps/guessing_game-d3f7e58244687f0a.rmeta: src/main.rs
-
-/Users/gleblevin/Projects/RustHomeWorks/guessing_game/target/debug/deps/guessing_game-d3f7e58244687f0a.d: src/main.rs
-
-src/main.rs:
