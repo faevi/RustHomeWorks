53cb94314b5afce4d9fdbd026093009f25bb9eb9
gleblevin <gleb-levin323@mail.ru>
2023-07-16 19:44:56 +0300

    Minor changes

--- my_market/programs/my_market/src/lib.rs
+++ my_market/programs/my_market/src/lib.rs
@@ -4,71 +4,65 @@ mod buyers;
 mod admins;
 mod common;
 
+use anchor_lang::prelude::*;
+use crate::products::inventory::MyInventory;
+use crate::products::pricing::Pricing;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+use crate::sellers::Sellers;
+use crate::buyers::Buyers;
+use crate::admins::Admins;
+use crate::common::Common;
+
+
+declare_id!("9vRQ68z2Mz5eZkneiwTDMni9AHtfCEV72HRgQgY7Jsap");
+
+#[account]
+#[derive(Default)]
+pub struct Market {
+pub inventory: MyInventory,
+pub pricing: Pricing,
+pub sellers: Sellers,
+pub buyers: Buyers,
+pub admins: Admins,
+pub common: Common,
+}
+
+#[program]
 pub mod my_market {
-    use anchor_lang::prelude::*;
-    use crate::products::inventory::MyInventory;
-    use crate::products::pricing::Pricing;
-    use anchor_lang::solana_program::entrypoint::ProgramResult;
-    use crate::sellers::Sellers;
-    use crate::buyers::Buyers;
-    use crate::admins::Admins;
-    use crate::common::Common;
+use super::*;
 
-    declare_id!("9vRQ68z2Mz5eZkneiwTDMni9AHtfCEV72HRgQgY7Jsap");
+pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    // Инициализация вашего умного контракта
+    let market = &mut ctx.accounts.market;
+    market.inventory = MyInventory::new();
+    market.pricing = Pricing::new();
+    market.sellers = Sellers::new();
+    market.buyers = Buyers::new();
+    market.admins = Admins::new();
+    market.common = Common::new();
 
-    
+    Ok(())
+}
 
-    #[program]
-    pub mod my_market {
-        use super::*;
 
-        pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-            // Инициализация вашего умного контракта
-            let market = &mut ctx.accounts.market;
-            market.inventory = MyInventory::new();
-            market.pricing = Pricing::new();
-            market.sellers = Sellers::new();
-            market.buyers = Buyers::new();
-            market.admins = Admins::new();
-            market.common = Common::new();
 
-            Ok(())
-        }
 
-        #[account]
-        pub struct Market {
-            pub inventory: MyInventory,
-            pub pricing: Pricing,
-            pub sellers: Sellers,
-            pub buyers: Buyers,
-            pub admins: Admins,
-            pub common: Common,
-        }
+#[access_control]
+pub fn add_product(ctx: Context<instruction::AddProduct>, product: crate::products::inventory::Product) -> ProgramResult {
+    // Добавление продукта в инвентарь
+    let market = &mut ctx.accounts.market;
+    market.inventory.add_product(product)?;
 
-        
-        #[access_control]
-        pub fn add_product(ctx: Context<AddProduct>, product: crate::products::inventory::Product) -> ProgramResult {
-            // Добавление продукта в инвентарь
-            let market = &mut ctx.accounts.market;
-            market.inventory.add_product(product)?;
+    Ok(())
+}
 
-            Ok(())
-        }
-        
 
-        #[account(init, payer = user)]
-        pub struct Initialize<'info> {
-            pub market: AccountInfo<'info>,
-            pub rent: Sysvar<'info, Rent>,
-        }
-        
-
-        #[derive(Accounts)]
-        pub struct AddProduct<'info> {
-            #[account(mut)]
-            pub market: AccountInfo<'info>,
-        }
-
-    }
+#[account(init, payer = user)]
+pub struct Initialize<'info> {
+    pub market: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
+}
 
 }
+
+
--- my_market/programs/my_market/src/main.rs
+++ my_market/programs/my_market/src/main.rs
@@ -1,8 +1,8 @@
 use anchor_lang::prelude::*;
-use my_market::my_market::MyMarket;
+use my_market::my_market::Market;
 
 fn main() {
     // Инициализация Anchor
-    let program = anchor_lang::solana_program::program::entrypoint::<MyMarket>();
+    let program = anchor_lang::solana_program::program::entrypoint::<Market>();
     program::invoke_entrypoint(program);
 }
